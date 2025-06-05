5e472ee60407108d1e569f8e7a7e125ef4861e90
Subramanyakoushik <psubramanya294@gmail.com>
2025-06-05 23:35:36 +0530

    Linkedlist

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+  public static void main(String[]args){
+    Linkedlist list = new Linkedlist();
+    list.insertEnd(6);
+     list.insertEnd(5);
+      list.insertEnd(3);
+       list.insertEnd(4);
+       list.insertEnd(2);
+        list.insertBefore(1);
+         list.insertBefore(0);
+         list.insertAtPos(8,2);
+         list.insertAtPos(50,4);
+         list.delbefore();
+         list.delbefore();
+        System.out.println(list.length()); 
+    list.print();
+}
+}
